3a2a32be1729f11db9371f749dfc7e9c945dd1b5
Daniel Chen <113078548+dchen024@users.noreply.github.com>
2024-03-02 17:46:14 -0500

    add dynamic lesson routes

---
+++ src/app/home/lesson/[id]/page.tsx
@@ -0,0 +1,9 @@
+export default function Page({ params }: { params: { id: number } }) {
+  console.log({ params });
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Lesson {params.id}</h1>
+    </main>
+  );
+}
