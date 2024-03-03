590061843dba6d26a56eec8e6fdbf279e2b825e8
Vansh Bataviya <bataviyavansh@gmail.com>
2024-03-03 07:30:51 -0500

    fix: progress import

--- src/components/quiz-component.tsx
+++ src/components/quiz-component.tsx
@@ -1,6 +1,7 @@
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { Progress } from "@/components/ui/progress";
 
 export function QuizComponent() {
   return (
