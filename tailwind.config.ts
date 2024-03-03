c6b0b833faeb6e63fc3ccfbf0c1096e5e2e18aab
Daniel Chen <113078548+dchen024@users.noreply.github.com>
2024-03-02 22:47:22 -0500

    fix merge error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,19 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
+    fontFamily: {
+      "din-rounded": "'DIN Next Rounded LT W01 Regular'",
+      "feather-bold": "'Feather Bold'",
+    },
     container: {
       center: true,
       padding: "2rem",
@@ -75,6 +79,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
